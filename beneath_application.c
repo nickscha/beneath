37dbef7b7b4be9d7b137ec4be5c881501a064a67
nickscha <nick@scharnbeck.de>
2025-10-20 12:47:46 +0200

    [CONF] move dependencies to deps folder

--- beneath_application.c
+++ beneath_application.c
@@ -1,7 +1,50 @@
 #include "beneath.h"
-#include "vm.h"
-#include "sb.h"
-#include "camera.h"
+#include "deps/vm.h"
+#include "deps/sb.h"
+
+typedef struct camera
+{
+    v3 position;
+    v3 front;
+    v3 up;
+    v3 right;
+    v3 worldUp;
+
+    float yaw;
+    float pitch;
+    float fov;
+
+} camera;
+
+void camera_update_vectors(camera *cam)
+{
+    float yawRadians = vm_radf(cam->yaw);
+    float pitchRadians = vm_radf(cam->pitch);
+    float pitchRadiansCos = vm_cosf(pitchRadians);
+
+    cam->front.x = vm_cosf(yawRadians) * pitchRadiansCos;
+    cam->front.y = vm_sinf(pitchRadians);
+    cam->front.z = vm_sinf(yawRadians) * pitchRadiansCos;
+    cam->front = vm_v3_normalize(cam->front);
+    cam->right = vm_v3_normalize(vm_v3_cross(cam->front, cam->worldUp));
+    cam->up = vm_v3_normalize(vm_v3_cross(cam->right, cam->front));
+}
+
+static camera camera_init(void)
+{
+    camera cam = {0};
+
+    cam.position = vm_v3_zero;
+    cam.front = vm_v3(0.0f, 0.0f, -1.0f);
+    cam.up = vm_v3(0.0f, 1.0f, 0.0f);
+    cam.worldUp = cam.up;
+    cam.fov = 45.0f;
+    cam.yaw = -90.0f;
+
+    camera_update_vectors(&cam);
+
+    return (cam);
+}
 
 static float cube_vertices[] = {
     -0.5f, -0.5f, -0.5f, /* Bottom-left back */
---
+++ deps/README.md
@@ -0,0 +1,16 @@
+# Dependencies 
+ 
+These dependencies are used by this projects tests/example codes. 
+ 
+All dependencies must fulfill these requirements: 
+- C89 standard 
+- nostdlib (No C Standard Library) 
+- single header library 
+- cross platform (Windows, Linux, MacOs) 
+- open source license (MIT, Apache, ...) 
+ 
+--- 
+ 
+List of dependencies used: 
+- sb.h: https://github.com/nickscha/sb - last updated: 2025-09-05 11:12:41 
+- vm.h: https://github.com/nickscha/vm - last updated: 2025-09-05 11:12:41 
